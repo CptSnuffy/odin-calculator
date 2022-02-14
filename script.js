0eb366ec785e2f2650a0622d810f0969e09cbb9b
Jacob Hutchinson <jhutchinson.programmer@gmail.com>
2022-02-14 15:35:46 -0500

    Finished logic for calculating, relying on string-math lib does not use eval function

--- script.js
+++ script.js
@@ -1,44 +1,78 @@
-function add(numOne, numTwo)
+
+// function add(numOne, numTwo)
+// {
+//     return numOne + numTwo;
+// }
+
+// function subtract(numOne, numTwo)
+// {
+//     return numOne - numTwo;
+// }
+
+// function multiply(numOne, numTwo)
+// {
+//     return numOne * numTwo;
+// }
+
+// function divide(numOne, numTwo)
+// {
+//     return numOne / numTwo;
+// }
+
+// function operate(operator, numOne = 0, numTwo = 0)
+// {
+//    switch(operator)
+//    {
+//         case '+':
+//             return add(numOne,numTwo);
+        
+//         case '-':
+//             return subtract(numOne,numTwo);
+        
+//         case '*':
+//             return multiply(numOne,numTwo);
+        
+//         case '/':
+//             return divide(numOne,numTwo);
+        
+//         default:
+//             console.log("Invalid operation");
+//    }
+// }
+
+function operate(equation)
 {
-    return numOne + numTwo;
+    return stringMath(equation);
 }
 
-function subtract(numOne, numTwo)
+function displayText(character, isOperator=false)
 {
-    return numOne - numTwo;
+    let display = document.getElementById('numberdisplay');
+    if ((display.textContent.endsWith('+') && isOperator) || (display.textContent.endsWith('-') && isOperator) || (display.textContent.endsWith('*') && isOperator) || (display.textContent.endsWith('/') && isOperator) || (display.textContent.endsWith('.') && isOperator))
+    {
+        console.log(display.textContent[display.textContent.length -1]);
+        display.textContent = display.textContent.slice(0,-1);
+        display.textContent += character;
+    }
+
+    else
+    {
+        display.textContent += character;
+    }
 }
 
-function multiply(numOne, numTwo)
+function clearText()
 {
-    return numOne * numTwo;
+    let display = document.getElementById('numberdisplay');
+    display.textContent = '';
 }
 
-function divide(numOne, numTwo)
+function parseNumbers()
 {
-    return numOne / numTwo;
-}
+    let equation = document.getElementById('numberdisplay');
+    equationToString = equation.innerHTML;
+    let splitEquation = equationToString.split(/(\d+\D+^\d)/);
+    let answer = operate(splitEquation[0]);
+    equation.innerHTML = answer;
 
-function operate(operator, numOne, numTwo)
-{
-   switch(operator)
-   {
-        case '+':
-            add(numOne,numTwo);
-            break;
-        
-        case '-':
-            subtract(numOne,numTwo);
-            break;
-        
-        case '*':
-            multiply(numOne,numTwo);
-            break;
-        
-        case '/':
-            divide(numOne,numTwo);
-            break;
-        
-        default:
-            console.log("Invalid operation");
-   }
 }
