5d35b998398de0c869080ba3deb1cdc5f0b897bf
Jacob Hutchinson <jhutchinson.programmer@gmail.com>
2022-03-17 12:39:14 -0400

    added css :)

--- script.js
+++ script.js
@@ -42,12 +42,32 @@
 
 function operate(equation)
 {
-    return stringMath(equation);
+    try
+    {
+        answer = stringMath(equation);
+        answer = parseFloat(answer).toFixed(2);
+        return answer;
+    }
+
+    catch(e)
+    {
+        console.log(e);
+        return 'Nice try you silly goober :)';
+    }
 }
 
 function displayText(character, isOperator=false)
 {
     let display = document.getElementById('numberdisplay');
+
+    if(character == 'back')
+    {
+        let str = display.textContent
+        str = str.substring(0, str.length-1);
+        display.textContent = str;
+        return;
+    }
+
     if ((display.textContent.endsWith('+') && isOperator) || (display.textContent.endsWith('-') && isOperator) || (display.textContent.endsWith('*') && isOperator) || (display.textContent.endsWith('/') && isOperator) || (display.textContent.endsWith('.') && isOperator))
     {
         console.log(display.textContent[display.textContent.length -1]);
@@ -61,6 +81,93 @@ function displayText(character, isOperator=false)
     }
 }
 
+let keyboardListener = document.addEventListener('keydown', function (key) {
+    switch(key.key)
+    {
+        case '1':
+            displayText(key.key, false);
+            break;
+
+        case '2':
+            displayText(key.key, false);
+            break;
+
+        case '3':
+            displayText(key.key, false);
+            break;
+
+        case '4':
+            displayText(key.key, false);
+            break;
+
+        case '5':
+            displayText(key.key, false);
+            break;
+            
+        case '6':
+            displayText(key.key, false);
+            break;
+
+        case '7':
+            displayText(key.key, false);
+            break;
+
+        case '8':
+            displayText(key.key, false);
+            break;
+
+        case '9':
+            displayText(key.key, false);
+            break;
+
+        case '0':
+            displayText(key.key, false);
+            break;
+
+        case '+':
+            displayText(key.key, true);
+            break;
+
+        case '-':
+            displayText(key.key, true);
+            break;
+
+        case '*':
+            displayText(key.key, true);
+            break;
+
+        case '/':
+            displayText(key.key, true);
+            break;
+
+        case '(':
+            displayText(key.key, false);
+            break;
+        case ')':
+            displayText(key.key, false);
+            break;
+
+        case '.':
+            displayText(key.key, true);
+            break;
+        
+        case 'Backspace':
+            displayText('back', false);
+            break;
+        
+        case 'Delete':
+            displayText('back', false);
+            break;
+        
+        case 'Enter':
+            parseNumbers();
+            break;
+            
+        default:
+            return
+    }
+});
+
 function clearText()
 {
     let display = document.getElementById('numberdisplay');
@@ -73,6 +180,8 @@ function parseNumbers()
     equationToString = equation.innerHTML;
     let splitEquation = equationToString.split(/(\d+\D+^\d)/);
     let answer = operate(splitEquation[0]);
+    //answer = parseFloat(answer).toFixed(2);
+    console.log(answer);
     equation.innerHTML = answer;
 
 }
